814afcc662171adf4975f4dbe1a670c296d9a696
Jonni Rainisto <jonni.rainisto@gmail.com>
2017-02-08 00:49:31 +0200

    Add support for swarn mode DNS name resolution

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sed -i 's/127.0.0.1/'"$NODE_IP"'/g' /emqttd/etc/emq.conf
+NODE=$(dig +short $NODE_IP)
+sed -i 's/127.0.0.1/'"$NODE"'/g' /emqttd/etc/emq.conf
 /emqttd/bin/emqttd start
 sleep 5
 if [ -z ${MASTER+x} ]; then
 echo '$MASTER' is not set.
 else
-/emqttd/bin/emqttd_ctl cluster join emqttd@$MASTER;
+CLUSTER=$(dig +short $MASTER)
+/emqttd/bin/emqttd_ctl cluster join emqttd@$CLUSTER
 fi
 sleep 10 && tail -f --retry /emqttd/log/*
