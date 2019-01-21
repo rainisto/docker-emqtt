52a57b4b5e4cc2406ad0b9734abe184f783d461f
Jonni Rainisto <jonni.rainisto@gmail.com>
2019-01-21 14:30:20 +0200

    add ip support

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NODE=$(dig +short $NODE_IP)
+if [ x"$NODE" == "x" ]; then NODE=$NODE_IP; fi
 sed -i 's/127.0.0.1/'"$NODE"'/g' /emqttd/etc/emq.conf
 /emqttd/bin/emqttd start
 sleep 5
@@ -7,6 +8,7 @@ if [ -z ${MASTER+x} ]; then
 echo '$MASTER' is not set.
 else
 CLUSTER=$(dig +short $MASTER)
+if [ x"$CLUSTER" == "x" ]; then CLUSTER=$MASTER; fi
 /emqttd/bin/emqttd_ctl cluster join emqttd@$CLUSTER
 fi
 sleep 10 && tail -f --retry /emqttd/log/*
